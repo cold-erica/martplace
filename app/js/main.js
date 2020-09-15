1f44869d035e8296b188ed41530ce557aba63256
cold-erica <cold.erica@gmail.com>
2020-09-15 17:09:20 +0300

    search slider product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,25 @@
-$(function(){
+$(function () {
+    var mixer = mixitup('.product__inner-box');
+    var mySwiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        slidesPerView: 1,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "12px"
+      });
 
 
 
-
-    
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ let gulp = require('gulp'),
     browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
-    cssmin = require('gulp-cssmin')
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin');
+
 const { version } = require('os');
 const { css } = require('jquery');
 
@@ -24,17 +25,20 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
     .pipe(concat('libs.min.css'))
-    .pipe(cssmi())
+    .pipe(cssmin())
     .pipe(gulp.dest('app/css'))
 });
 
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())

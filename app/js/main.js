e1cd311eb65427e6d6a0b6e34da1a6837b02b8bb
cold-erica <cold.erica@gmail.com>
2020-09-16 01:17:05 +0300

    slider,rate-star,followers

--- app/js/main.js
+++ app/js/main.js
@@ -15,11 +15,29 @@ $(function () {
         },
     })
 
+    var mySwiper2 = new Swiper('.followers__swiper-container', {
+        direction: 'horizontal',
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
     $(".rate-star").rateYo({
         rating: 4.5,
-        starWidth: "12px"
-      });
-
+        starWidth: "12px",
+        normalFill: "#ffffff",
+        readOnly: true
+    });
+    
 
 
 });

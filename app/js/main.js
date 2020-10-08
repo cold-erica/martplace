3cead3a38b1b43bba1e60cf49a1ba516165f182a
cold-erica <cold.erica@gmail.com>
2020-10-08 15:24:26 +0300

    single-product,favorits

--- app/js/main.js
+++ app/js/main.js
@@ -47,7 +47,7 @@ $(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        
+
     });
 
 
@@ -57,33 +57,33 @@ $(function () {
         normalFill: "#ffffff",
         readOnly: true
     });
-    
-    $(".header__menu-button").on("click",function(){
+
+    $(".header__menu-button").on("click", function () {
         $(".nav .nav__list").slideToggle();
     });
-    $(".nav__link-home").on("click",function(){
+    $(".nav__link-home").on("click", function () {
         $(".drop-menu__home").slideToggle();
     });
-    $(".nav__link-products").on("click",function(){
+    $(".nav__link-products").on("click", function () {
         $(".drop-menu__products").slideToggle();
     });
-    $(".nav__link-wordpress").on("click",function(){
+    $(".nav__link-wordpress").on("click", function () {
         $(".drop-menu__wordpress").slideToggle();
     });
-    $(".header__account").on("click",function(){
+    $(".header__account").on("click", function () {
         $(".drop-menu__account").slideToggle();
     });
-    $(".menu__alarm").on("click",function(){
+    $(".menu__alarm").on("click", function () {
         $(".drop-menu__alarm").slideToggle();
     });
-    $(".menu__mail").on("click",function(){
+    $(".menu__mail").on("click", function () {
         $(".drop-menu__messages").slideToggle();
     });
-    $(".menu__cart").on("click",function(){
+    $(".menu__cart").on("click", function () {
         $(".drop-menu__cart").slideToggle();
     });
-    $(".menu__pages").on("click",function(){
+    $(".menu__pages").on("click", function () {
         $(".drop-menu__pages").slideToggle();
     });
-    
+
 });
